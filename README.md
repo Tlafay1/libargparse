329e8e62c3854d9d696c8e02a95751ba2f85908f
Tlafay1 <tlclash12@gmail.com>
2023-11-21 10:40:30 +0100

    Basic doc done

--- README.md
+++ README.md
@@ -31,9 +31,95 @@ $(CC) $(OBJDIR) ./libargparse/libargparse.a -o $(NAME) $(CFLAGS) -Llibft -lft -W
 
 That's it !
 
-# Usage
+# Example usage
 
-Coming soon TM ;)
+```C
+#include "argparse.h"
+
+/**
+ * This array of structures holds the informations about the args expected.
+ * The last element of the array must be {0}.
+ **/
+static t_argo options[] = {
+	{'v', "verbose", "verbose", "Enable verbose mode", NO_ARG},
+	{'t', "test", "test", "Display a test", NO_ARG},
+	{'o', "output", "output", "Specify output file", ONE_ARG},
+	{'i', "input", "input", "Specify input file", INF_ARG},
+	{0}
+};
+
+/**
+ * This structure holds the informations about the parser.
+ * The members are:
+ * - options: the array of structures that holds the informations about the args expected.
+ * - args_doc: a string that describes the non-option command-line arguments to use in the help menu.
+ * - doc: a string that describes the program to the user in the help menu.
+ **/
+
+static t_argp argp = {
+	.options = options,
+	.args_doc = "INPUT_FILE OUTPUT_FILE",
+	.doc = "This is a sample program to demonstrate how to use argp to parse command line arguments.",
+};
+
+int main(int argc, char const *argv[])
+{
+	// Parse the arguments and store them in a linked list.
+	t_list *head = parse_args(&argp, argc, argv);
+	t_argr *argr;
+
+	// Loop through the options (e.g ./program -abc --option).
+	while ((argr = get_next_option(head)))
+	{
+		// Be careful, the option can be NULL.
+		if (argr->option)
+		{
+			printf("sflag: %c, lflag: %s, name: %s\n", argr->option->sflag,
+				   argr->option->lflag, argr->option->name);
+		}
+		// Be careful, the values can be NULL.
+		if (argr->values)
+			for (int i = 0; argr->values[i]; i++)
+				printf("%s\n", argr->values[i]);
+	}
+
+	// Loop through the arguments (e.g ./program arg1 arg2 arg3).
+	while ((argr = get_next_arg(head)))
+	{
+		// Be careful, the option can be NULL.
+		if (argr->option)
+		{
+			printf("sflag: %c, lflag: %s, name: %s\n", argr->option->sflag,
+				   argr->option->lflag, argr->option->name);
+		}
+		// Be careful, the values can be NULL.
+		if (argr->values)
+			for (int i = 0; argr->values[i]; i++)
+				printf("%s\n", argr->values[i]);
+	}
+
+	// IMPORTANT: don't forget to free the linked list !
+	free_args(head);
+
+	return 0;
+}
+```
+
+Output:
+
+```bash
+./test bonjour au revoir -i test -o test -v -v -v
+sflag: i, lflag: input, name: input
+test
+sflag: o, lflag: output, name: output
+test
+sflag: v, lflag: verbose, name: verbose
+sflag: v, lflag: verbose, name: verbose
+sflag: v, lflag: verbose, name: verbose
+bonjour
+au
+revoir
+```
 
 ---
 
--- argparse.c
+++ argparse.c
@@ -12,22 +12,6 @@
 
 #include "argparse.h"
 
-static void	_free_args(t_list *head)
-{
-	t_list	*tmp;
-	t_argr	*argr;
-
-	while (head)
-	{
-		tmp = head->next;
-		argr = head->content;
-		free(argr->values);
-		free(argr);
-		free(head);
-		head = tmp;
-	}
-}
-
 static int _unrecognized_option(char sflag, char *lflag, const char *progname)
 {
 	if (sflag)
@@ -36,16 +20,15 @@ static int _unrecognized_option(char sflag, char *lflag, const char *progname)
 		printf("%s: unrecognized option '--%s'\n", progname, lflag);
 
 	printf("Try '%s --help' for more information\n", progname);
-	
+
 	return (1);
 }
 
-static t_argo	*_search_option(char sflag, char *lflag, t_argo *options)
+static t_argo *_search_option(char sflag, char *lflag, t_argo *options)
 {
 	for (t_argo *option = options; option->sflag || option->lflag; option++)
 	{
-		if ((sflag && sflag == option->sflag)
-			|| (lflag && !ft_strncmp(lflag, option->lflag, ft_strlen(lflag))))
+		if ((sflag && sflag == option->sflag) || (lflag && !ft_strncmp(lflag, option->lflag, ft_strlen(lflag))))
 			return (option);
 	}
 	return (NULL);
@@ -53,7 +36,7 @@ static t_argo	*_search_option(char sflag, char *lflag, t_argo *options)
 
 static int _wrong_arguments_number(t_argo *option, int flagtype, const char *progname)
 {
-	char	*message;
+	char *message;
 
 	if (option->argnum == ONE_ARG)
 		message = "an";
@@ -71,10 +54,10 @@ static int _wrong_arguments_number(t_argo *option, int flagtype, const char *pro
 	return (1);
 }
 
-static int	_get_option_arguments(char ***args, t_argo *option,
-	char ***values, int flagtype, const char *progname)
+static int _get_option_arguments(char ***args, t_argo *option,
+								 char ***values, int flagtype, const char *progname)
 {
-	char	**values_cp = *values;
+	char **values_cp = *values;
 
 	if (option->argnum == NO_ARG)
 		values_cp = NULL;
@@ -83,7 +66,7 @@ static int	_get_option_arguments(char ***args, t_argo *option,
 	{
 		if (!*args || *args[0][0] == '-')
 			return (_wrong_arguments_number(option, flagtype, progname));
-		values_cp = (char **) malloc(2 * sizeof(char *));
+		values_cp = (char **)malloc(2 * sizeof(char *));
 		values_cp[0] = **args;
 		values_cp[1] = NULL;
 		(*args)++;
@@ -94,10 +77,11 @@ static int	_get_option_arguments(char ***args, t_argo *option,
 		if (!**args)
 			return (_wrong_arguments_number(option, flagtype, progname));
 		int i = 0;
-		for (; (*args)[i] && (ft_strlen((*args)[i]) <= 1 || (*args)[i][0] != '-'); i++);
+		for (; (*args)[i] && (ft_strlen((*args)[i]) <= 1 || (*args)[i][0] != '-'); i++)
+			;
 		if (!i)
 			return (_wrong_arguments_number(option, flagtype, progname));
-		values_cp = (char **) malloc((i + 1) * sizeof(char *));
+		values_cp = (char **)malloc((i + 1) * sizeof(char *));
 		values_cp[i] = NULL;
 		for (int j = 0; j < i; j++)
 			values_cp[j] = (*args)[j];
@@ -110,15 +94,15 @@ static int	_get_option_arguments(char ***args, t_argo *option,
 
 static int _parse_short_option(char ***args, t_list **head, t_argo *options, const char *progname)
 {
-	t_argo	*option;
-	t_argr	*ret;
+	t_argo *option;
+	t_argr *ret;
 
 	char *arg = (*args)[0] + 1;
 	(*args)++;
 
 	for (; *arg; arg++)
 	{
-		ret = (t_argr *) malloc(sizeof(t_argr));
+		ret = (t_argr *)malloc(sizeof(t_argr));
 		option = _search_option(*arg, NULL, options);
 
 		if (!option)
@@ -134,13 +118,13 @@ static int _parse_short_option(char ***args, t_list **head, t_argo *options, con
 
 static int _parse_long_option(char ***args, t_list **head, t_argo *options, const char *progname)
 {
-	t_argo	*option;
-	t_argr	*ret;
+	t_argo *option;
+	t_argr *ret;
 
 	char *arg = (*args)[0] + 2;
 	(*args)++;
 
-	ret = (t_argr *) malloc(sizeof(t_argr));
+	ret = (t_argr *)malloc(sizeof(t_argr));
 	option = _search_option(0, arg, options);
 
 	if (!option)
@@ -160,7 +144,7 @@ static int _parse_option(char ***args, t_list **head, t_argo *options, const cha
 	return _parse_short_option(args, head, options, progname);
 }
 
-t_argr	*get_next_arg(t_list *head)
+t_argr *get_next_arg(t_list *head)
 {
 	static t_list *current = NULL;
 	static int end = 0;
@@ -170,7 +154,7 @@ t_argr	*get_next_arg(t_list *head)
 
 	if (!current)
 		current = head;
-	
+
 	while (current && ((t_argr *)current->content)->option)
 		current = current->next;
 	if (!current)
@@ -185,7 +169,23 @@ t_argr	*get_next_arg(t_list *head)
 	return (ret);
 }
 
-t_argr	*get_next_option(t_list *head)
+void free_args(t_list *head)
+{
+	t_list *tmp;
+	t_argr *argr;
+
+	while (head)
+	{
+		tmp = head->next;
+		argr = head->content;
+		free(argr->values);
+		free(argr);
+		free(head);
+		head = tmp;
+	}
+}
+
+t_argr *get_next_option(t_list *head)
 {
 	static t_list *current = NULL;
 	static int end = 0;
@@ -195,7 +195,7 @@ t_argr	*get_next_option(t_list *head)
 
 	if (!current)
 		current = head;
-	
+
 	while (current && !((t_argr *)current->content)->option)
 		current = current->next;
 	if (!current)
@@ -210,9 +210,9 @@ t_argr	*get_next_option(t_list *head)
 	return (ret);
 }
 
-void	help_args(t_argp *argp, const char *prog_name)
+void help_args(t_argp *argp, const char *prog_name)
 {
-	t_argo	*options = argp->options;
+	t_argo *options = argp->options;
 
 	if (argp->args_doc)
 		printf("Usage: %s %s\n", prog_name, argp->args_doc);
@@ -226,10 +226,10 @@ void	help_args(t_argp *argp, const char *prog_name)
 	printf("  -%c, --%-17s%s\n", 'h', "help", "display this help and exit");
 }
 
-t_list	*parse_args(t_argp *argp, int argc, char const *argv[])
+t_list *parse_args(t_argp *argp, int argc, char const *argv[])
 {
-	t_list	*head;
-	t_argr	*ret;
+	t_list *head;
+	t_argr *ret;
 
 	(void)argc;
 	t_argo *options = argp->options;
@@ -245,10 +245,10 @@ t_list	*parse_args(t_argp *argp, int argc, char const *argv[])
 		}
 		else
 		{
-			ret = (t_argr *) malloc(sizeof(t_argr));
+			ret = (t_argr *)malloc(sizeof(t_argr));
 			ret->option = NULL;
 			char **tmp = (char **)malloc(2 * sizeof(char *));
-			tmp[0] = (char *) *argv;
+			tmp[0] = (char *)*argv;
 			tmp[1] = NULL;
 			ret->values = tmp;
 			ft_lstadd_back(&head, ft_lstnew(ret));
